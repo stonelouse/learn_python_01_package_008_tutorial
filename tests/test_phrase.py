5ed21985292fb2b20919629eda038db900309151
Christian Russ <c.russ@web.de>
2025-11-01 07:27:40 +0100

    feat: Test suite first version

--- tests/test_phrase.py
+++ tests/test_phrase.py
@@ -1,2 +1,10 @@
+## Our test suite for the Phrase class
+
 # Importing 'Phrase' in the test file
 from palindrome_stonelouse.phrase import Phrase
+
+def test_non_palindrome():
+    assert not Phrase("apple").ispalindrome()
+
+def test_literal_palindrome():
+    assert Phrase("racecar").ispalindrome()
