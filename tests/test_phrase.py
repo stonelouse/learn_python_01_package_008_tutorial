700ccd11eaabdfc6ccf8d802cae73d7374cc49b1
Christian Russ <c.russ@web.de>
2025-11-01 07:45:33 +0100

    feat: Test suite second version
    Pending and further tests cases added

--- tests/test_phrase.py
+++ tests/test_phrase.py
@@ -1,10 +1,21 @@
 ## Our test suite for the Phrase class
 
 # Importing 'Phrase' in the test file
+from unittest import skip
 from palindrome_stonelouse.phrase import Phrase
+
 
 def test_non_palindrome():
     assert not Phrase("apple").ispalindrome()
 
+
 def test_literal_palindrome():
     assert Phrase("racecar").ispalindrome()
+
+
+def test_mixed_case_palindrome():
+    assert Phrase("RaceCar").ispalindrome()
+
+
+def test_palindrome_with_punctuation():
+    skip("Not implemented yet")
